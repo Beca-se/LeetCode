8731e3bd3e2e87f7719643eefd539b2603dcbb20
zhouhu <zhou.hu@stee.stengg.com.cn>
2022-04-04 13:43:54 +0800

    [ZH-744]: https://leetcode-cn.com/problemset/all/ [Desc]: Leetcode 307的题解

---
+++ src/main/java/leetcode/editor/cn/[307]区域和检索 - 数组可修改.java
@@ -0,0 +1,110 @@
+//给你一个数组 nums ，请你完成两类查询。 
+//
+// 
+// 其中一类查询要求 更新 数组 nums 下标对应的值 
+// 另一类查询要求返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 ，其中 left <= right 
+// 
+//
+// 实现 NumArray 类： 
+//
+// 
+// NumArray(int[] nums) 用整数数组 nums 初始化对象 
+// void update(int index, int val) 将 nums[index] 的值 更新 为 val 
+// int sumRange(int left, int right) 返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元
+//素的 和 （即，nums[left] + nums[left + 1], ..., nums[right]） 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：
+//["NumArray", "sumRange", "update", "sumRange"]
+//[[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+//输出：
+//[null, 9, null, 8]
+//
+//解释：
+//NumArray numArray = new NumArray([1, 3, 5]);
+//numArray.sumRange(0, 2); // 返回 1 + 3 + 5 = 9
+//numArray.update(1, 2);   // nums = [1,2,5]
+//numArray.sumRange(0, 2); // 返回 1 + 2 + 5 = 8
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 104 
+// -100 <= nums[i] <= 100 
+// 0 <= index < nums.length 
+// -100 <= val <= 100 
+// 0 <= left <= right < nums.length 
+// 调用 update 和 sumRange 方法次数不大于 3 * 104 
+// 
+// Related Topics 设计 树状数组 线段树 数组 
+// 👍 403 👎 0
+package leetcode.editor.cn;
+
+class RangeSumQueryMutable {
+    public static void main(String[] args) {
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class NumArray {
+        private int[] sum; // sum[i] 表示第 i 个块的元素和
+        private int size; // 块的大小
+        private int[] nums;
+
+        public NumArray(int[] nums) {
+            this.nums = nums;
+            int n = nums.length;
+            size = (int) Math.sqrt(n);
+            sum = new int[(n + size - 1) / size]; // n/size 向上取整
+            for (int i = 0; i < n; i++) {
+                sum[i / size] += nums[i];
+            }
+        }
+
+        public void update(int index, int val) {
+            sum[index / size] += val - nums[index];
+            nums[index] = val;
+        }
+
+        public int sumRange(int left, int right) {
+            int b1 = left / size, i1 = left % size, b2 = right / size, i2 = right % size;
+            if (b1 == b2) { // 区间 [left, right] 在同一块中
+                int sum = 0;
+                for (int j = i1; j <= i2; j++) {
+                    sum += nums[b1 * size + j];
+                }
+                return sum;
+            }
+            int sum1 = 0;
+            for (int j = i1; j < size; j++) {
+                sum1 += nums[b1 * size + j];
+            }
+            int sum2 = 0;
+            for (int j = 0; j <= i2; j++) {
+                sum2 += nums[b2 * size + j];
+            }
+            int sum3 = 0;
+            for (int j = b1 + 1; j < b2; j++) {
+                sum3 += sum[j];
+            }
+            return sum1 + sum2 + sum3;
+        }
+
+    }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+}
